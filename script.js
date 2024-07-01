704b5ec94b2fa8f162195799699d445bfbfe45a9
Gloria <osemudiamegloria51@gmail.com>
2024-07-01 16:06:00 +0100

    worked on responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+// scripts/menu-toggle.js
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.querySelector('.menu img');
     const nav = document.querySelector('nav');
     const headerBtn = document.querySelector('.header__btn');
-    const navUl = document.querySelector('nav ul');
 
     menuIcon.addEventListener('click', function() {
         nav.classList.toggle('toggled');
         headerBtn.classList.toggle('toggled');
-        navUl.classList.toggle('toggled');
     });
 });
